59dce776cfb8c90b03390a18b635e10d87dc091a
James Austen <aus.james@gmail.com>
2025-10-05 10:02:28 +0100

    refactor: use gamevals

--- src/main/java/com/brooklyn/tobnoticeboard/Constant.java
+++ src/main/java/com/brooklyn/tobnoticeboard/Constant.java
@@ -1,5 +1,7 @@
 package com.brooklyn.tobnoticeboard;
 
+import net.runelite.api.gameval.InterfaceID;
+
 public class Constant
 {
 	public static final String CONFIG_GROUP = "tobnoticeboard";
@@ -7,9 +9,9 @@ public class Constant
 	public static final String CONFIG_KEY_FRIEND_NOTES = "friendNotes";
 	public static final String CONFIG_KEY_ORB_ORDER_ENABLED = "orbOrderEnabled";
 
-	public static final int NOTICE_BOARD_COMPONENT_ID = 364;
-	public static final int LOBBY_COMPONENT_ID = 50;
-	public static final int TOB_HUD_COMPONENT_ID = 28;
+	public static final int NOTICE_BOARD_COMPONENT_ID = InterfaceID.TOB_PARTYLIST;
+	public static final int LOBBY_COMPONENT_ID = InterfaceID.TOB_PARTYDETAILS;
+	public static final int TOB_HUD_COMPONENT_ID = InterfaceID.TOB_HUD;
 	public static final int TOB_HUD_CHILD_COMPONENT_ID = 12;
 	public static final int TOB_MAX_PARTY_SIZE = 5;
 
